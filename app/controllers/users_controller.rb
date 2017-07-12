a93935f4889847c368f3b5144da875fb4397f8fd
kiyokuro <kuro6412@gmail.com>
2017-07-12 16:46:54 +0900

    ユーザを作成できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
 
-  def new do
-
+  def new
+    @user = User.new
   end
 
-  def create do
-    
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      flash[:info] = "アカウントの作成に成功しました。"
+    else
+      render 'new'
+    end
+
+    def show
+
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :slack_id, :password,
+                                 :password_confirmation)
   end
 end
