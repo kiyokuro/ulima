cdd7fe3ccd7c3af89f618a77f37f3daf7a5365d8
kiyokuro <kuro6412@gmail.com>
2017-07-12 17:21:53 +0900

    rubo修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -16,7 +15,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
 
   private
 
