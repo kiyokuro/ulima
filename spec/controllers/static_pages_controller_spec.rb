c43404551b6942e8157646f92036cf9469020050
kiyokuro <kuro6412@gmail.com>
2017-07-12 15:08:35 +0900

    rubo修正

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, :type => :controller do
-
   describe "GET home" do
     before { get :home }
     context 'ログイン前のユーザ' do
-      it {expect(response).to be_success}
-      it{expect(response).to render_template('home')}
+      it { expect(response).to be_success }
+      it { expect(response).to render_template('home') }
     end
     context 'ログイン済みのユーザ' do
-      
     end
   end
 end
