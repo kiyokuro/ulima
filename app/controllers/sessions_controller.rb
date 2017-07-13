ce755fe872e7a4394ecaf8fc97f52dc1b1792c03
kiyokuro <kuro6412@gmail.com>
2017-07-13 17:43:14 +0900

    ユーザログインを実現するためにフォームを追加しログイン処理を追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to(root_path)
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードに誤りがあります'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
---
+++ spec/requests/sessions_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, :type => :controller do
+
+  describe "#new" do
+    context 'アクセス成功' do
+      before{get :new}
+      it{expect(response).to be_success}
+    end
+  end
+
+  describe '#create' do
+    let!(:user){create(:user)}
+    context 'ログイン成功' do
+      before{post :create, params:{session:{email: 'example@example.com', password: 'password'}}}
+      it{expect(response).to redirect_to(root_path)}
+    end
+    context 'ログイン失敗' do
+      before{post :create, params:{session:{email: 'aaa@a.com', password: 'password'}}}
+      it{expect(response).to render_template('new')}
+    end
+  end
+
+end
