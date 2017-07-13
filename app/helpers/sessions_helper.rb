1278d24600bc95bb19f72de1bc47bcc26565bf4c
kiyokuro <kuro6412@gmail.com>
2017-07-13 18:35:46 +0900

    コードの可読性を上げるため、スペースを入れるよう修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,4 @@
 module SessionsHelper
-
   def login(user)
     session[:user_id] = user.id
   end
--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
-
   describe "#new" do
     context 'アクセス成功' do
-      before{get :new}
-      it{expect(response).to be_success}
+      before { get :new }
+      it { expect(response).to be_success }
     end
   end
 
   describe '#create' do
-    let!(:user){create(:user)}
+    let!(:user) { create(:user) }
     context 'ログイン成功' do
-      before{post :create, params:{session:{email: 'example@example.com', password: 'password'}}}
-      it{expect(response).to redirect_to(root_path)}
+      before { post :create, params: { session: { email: 'example@example.com', password: 'password' } } }
+      it { expect(response).to redirect_to(root_path) }
     end
     context 'ログイン失敗' do
-      before{post :create, params:{session:{email: 'aaa@a.com', password: 'password'}}}
-      it{expect(response).to render_template('new')}
+      before { post :create, params: { session: { email: 'aaa@a.com', password: 'password' } } }
+      it { expect(response).to render_template('new') }
     end
   end
-
 end
