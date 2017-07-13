a97833d59b8f70ec59b45de33041abdd375c4109
kiyokuro <kuro6412@gmail.com>
2017-07-13 18:33:01 +0900

    ログインを実現するため、セッションによりuser_idを記録するようにた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      login(user)
       redirect_to(root_path)
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードに誤りがあります'
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    logout
+    redirect_to(root_path)
   end
 end
