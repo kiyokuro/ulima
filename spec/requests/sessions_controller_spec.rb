7e06ab25a35efee4d71c717c0ea5b510df86e37a
kiyokuro <kuro6412@gmail.com>
2017-07-14 17:23:34 +0900

    ログイン、ログアウトのテスト追加

--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -13,10 +13,26 @@ RSpec.describe SessionsController, :type => :controller do
     context 'ログイン成功' do
       before { post :create, params: { session: { email: 'example@example.com', password: 'password' } } }
       it { expect(response).to redirect_to(root_path) }
+      it { expect(session[:user_id]).to eq user.id }
+      it { expect(cookies.permanent.signed[:user_id]).to eq user.id }
+      it { expect(cookies.permanent.signed[:remember_token]).not_to eq nil }
     end
     context 'ログイン失敗' do
       before { post :create, params: { session: { email: 'aaa@a.com', password: 'password' } } }
       it { expect(response).to render_template('new') }
+      it { expect(flash[:danger]).not_to be_empty }
+    end
+  end
+
+  describe '#destroy' do
+    let!(:user) { create(:user) }
+    context 'ログアウト成功' do
+      before do
+        post :create, params: { session: { email: 'example@example.com', password: 'password' } }
+        delete :destroy
+      end
+      it { expect(@current_user).to be nil }
+      it { expect(session[:user_id]).to be nil }
     end
   end
 end
