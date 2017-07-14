b52730405f9a1fa0cb5fd685c99f8cbbd8eb09fb
kiyokuro <kuro6412@gmail.com>
2017-07-14 11:00:41 +0900

    ブラウザを閉じてもログインを維持するため、モデルにトークンを保存

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       login(user)
+      remember(user)
       redirect_to(root_path)
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードに誤りがあります'
