89cd0a7581cdf5f9f20588d8122049b7f9264a02
kiyokuro <kuro6412@gmail.com>
2017-07-21 15:56:15 +0900

    アイテム購入とアイテム取得のテスト追加

--- spec/requests/purchases_controller_spec.rb
+++ spec/requests/purchases_controller_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PurchasesController, type: :request do
-
   let!(:user) { create(:user) }
   let(:item) { create(:item, param) }
   before { log_in_as(user) }
 
   describe 'create' do
     context 'アイテムを購入できた' do
-      let!(:param) { {quantity: 1} }
-      before { post purchases_path, params: {id: item.id} }
+      let!(:param) { { quantity: 1 } }
+      before { post purchases_path, params: { id: item.id } }
       it { expect(response).to render_template 'buy_success' }
     end
     context 'アイテムの購入に失敗した' do
-      let!(:param) { {quantity: 0}}
-      before { post purchases_path, params: {id: item.id} }
-      it { expect(response).to render_template 'items/soldout'}
+      let!(:param) { { quantity: 0 } }
+      before { post purchases_path, params: { id: item.id } }
+      it { expect(response).to render_template 'items/soldout' }
     end
   end
 
