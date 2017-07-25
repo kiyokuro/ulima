2129b472559e5f45a3ae82f8551303e8e8b3c9d0
kiyokuro <kuro6412@gmail.com>
2017-07-25 10:49:32 +0900

    メールのテスト追加

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,9 +1,6 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
   process tags: ['item']
   process convert: 'png'
