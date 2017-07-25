8b3f145d3b8828ba051da6a774ee0488ed57bed5
kiyokuro <kuro6412@gmail.com>
2017-07-25 15:56:25 +0900

    productionでlibが読み込まれないので読み込み方法を変更

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Ulima
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths += %W(#{config.root}/lib/validator)
+    config.eager_load_paths += %W(#{config.root}/lib/validator)
   end
 end
